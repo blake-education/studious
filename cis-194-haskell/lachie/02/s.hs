6c83e1ea8097bcbfa4ac8c92182aff5ebe810756
Lachie Cox <lachiec@gmail.com>
2015-03-02 20:04:34 +1100

    finish hw

--- cis-194-haskell/lachie/02/s.hs
+++ cis-194-haskell/lachie/02/s.hs
@@ -9,7 +9,6 @@ parseMessage ("W":(ts:msg)) = LogMessage Warning (read ts) (unwords msg)
 parseMessage ("E":(errno:(ts:msg))) = LogMessage (Error $ read errno) (read ts) (unwords msg)
 parseMessage x = Unknown (unwords x)
 
-
 parse :: String -> [LogMessage]
 parse s = map parseMessage . map words $ lines s
 
@@ -17,28 +16,28 @@ ltMessage :: LogMessage -> LogMessage -> Bool
 ltMessage (LogMessage _ ts1 _) (LogMessage _ ts2 _) = ts1 < ts2
 ltMessage _ _ = False
 
-gtTree :: LogMessage -> MessageTree -> MessageTree
-gtTree msg Leaf = Node Leaf msg Leaf
-gtTree (Unknown _) tree = tree
-gtTree msg (Node left treeMsg Leaf) = 
-gtTree msg (Node Leaf treeMsg right) = 
-gtTree msg (Node left treeMsg right) | (ltMessage msg treeMsg) = (gtTree msg (Node left treeMsg right))
-                                     | otherwise = (gtTree msg (Node left treeMsg right))
+insert :: LogMessage -> MessageTree -> MessageTree
+insert msg Leaf = Node Leaf msg Leaf
+insert (Unknown _) tree = tree
+insert msg (Node left treeMsg right) | (ltMessage msg treeMsg) = Node (insert msg left) treeMsg right
+                                     | otherwise = Node left treeMsg (insert msg right)
 
-{-ltTree :: LogMessage -> MessageTree -> MessageTree-}
-{-ltTree msg Leaf = Leaf-}
-{-ltTree (Unknown _) tree = tree-}
-{-ltTree msg@(LogMessage _ ts _) tree = Leaf-}
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
 
-{-insert :: LogMessage -> MessageTree -> MessageTree-}
-{-insert (Unknown _) tree = tree-}
-{-insert msg Leaf = Node Leaf msg Leaf-}
-{-insert msg tree = Node (ltTree msg tree) msg (gtTree msg tree)-}
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left msg right) = inOrder left ++ [msg] ++ inOrder right
 
+isBadMessage :: LogMessage -> Bool
+isBadMessage (LogMessage (Error severity) _ _) = severity >= 50
+isBadMessage _ = False
 
--- main = testParse parser 10 "sample.log"
+messageStrings :: [LogMessage] -> [String]
+messageStrings = map ( \(LogMessage _ _ msg) -> msg )
 
-{-main :: IO [LogMessage]-}
-{-main = testParse parse 20 "sample.log"-}
-main :: IO ()
-main = print $ ltMessage (LogMessage Info 12 "hi") (LogMessage Info 11 "hi")
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = messageStrings . filter isBadMessage . inOrder . build
+
+main :: IO [String]
+main = testWhatWentWrong parse whatWentWrong "error.log"
